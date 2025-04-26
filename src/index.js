c56b40fe0e5bc7107ba02fa3bcaaf4bc35f38433
Cloud <115522789+jkluio10@users.noreply.github.com>
2025-04-26 20:33:56 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<TemporaryComponent />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
